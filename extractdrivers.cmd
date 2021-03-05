62db9cc74bdc17d7a034360b2e8303aad91df1dc
Shannon Fritz <github@mrshannon.com>
2021-03-05 08:30:50 -0600

    Extrct drivers to their own subdir for each MSI

--- extractdrivers.cmd
+++ extractdrivers.cmd
@@ -4,23 +4,43 @@ REM ---
 REM Download drivers from the site below and save them someplace like c:\surfacedrivers\
 REM https://support.microsoft.com/en-us/help/4023482/surface-download-drivers-and-firmware-for-surface
 REM Copy this script to the same directory
-REM Open a cmd prompt in the same directory run the script
+REM Open a cmd prompt in the same directory run the script to extract then to a Drivers subdirectory
 REM ---
 
-REM Make a Drivers and a temporary "x" directory in the current directory
-if NOT EXIST "%CD%\x" ( mkdir "%CD%\x" )
+
+REM Make a Drivers directory in the current directory
 if NOT EXIST "%CD%\Drivers" ( mkdir "%CD%\Drivers" )
 
-REM Extract all the drivers to the "x" direcotry
-forfiles /M *.msi /C "cmd.exe /C echo @file && msiexec -a @file /qb targetdir=%CD%\x"
+REM loop through and process every .msi with a subroutine
+for %%f in (*.msi) do call :msifile %%~nf
+goto:end
 
-REM if you want to extract them in a specific order, then specify what to do below
-REM msiexec -a SurfaceGo_Win10_17763_1902010_WiFi_2.msi /qb targetdir="%CD%\x"
-REM msiexec -a SurfacePro4_Win10_18362_19.100.2166.0.msi /qb targetdir="%CD%\x"
-REM msiexec -a SurfacePro_Win10_18362_19.092.25297.0.msi /qb targetdir="%CD%\x"
+
+:msifile
+echo Processing %1
+
+REM Make a temporary eXtraction directory
+if NOT EXIST "%CD%\x" ( mkdir "%CD%\x" )
+
+if EXIST "%CD%\Drivers\%1" ( echo Deleting existing drivers for %1 && rmdir /s /q "%CD%\Drivers\%1" )
+
+REM Extrat the drivers from the msi
 REM msiexec -a SurfaceLaptop2_Win10_18362_19.100.3934.0.msi /qb targetdir="%CD%\x"
-REM msiexec -a SurfaceLaptop_Win10_18362_19.100.3933.0.msi /qb targetdir="%CD%\x"
+echo Extracting %1
+msiexec -a %1.msi /qb targetdir=%CD%\x
 
-REM move the extracted drivers to the "Drivers" directory
-for /D %%D in ("%CD%\x\SurfaceUpdate\*") do @echo %%D && move /Y %%D "%CD%\Drivers\"
-rmdir /s /q "%CD%\x"
+REM Make a subdirectory for this driver
+mkdir "%CD%\Drivers\%1"
+
+REM Move the extracted drivers
+echo "Moving extracted drivers for %1"
+for /D %%D in ("%CD%\x\SurfaceUpdate\*") do @echo %%D && move /Y %%D "%CD%\Drivers\%1
+
+REM Delete whatever else was left
+echo. Cleaing up after %1
+rmdir /s /q "%CD%\x""
+goto:eof
+
+
+:end
+echo. Done.
